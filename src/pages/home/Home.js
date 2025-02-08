c557ee98afefc7a23f809b1be69533d26a2c464d
Kiran Dighe <kiran.dighe@thehirehq.com>
2025-02-08 16:04:00 +0530

    changes

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../../styles/Home.scss";
 import student from "../../styles/assets/kiran2.png";
 
 function Home() {
+  const roles = [
+    "Full-Stack Developer",
+    "MERN Stack Developer",
+    "Software Engineer",
+    "Tech Enthusiast",
+  ];
+
+  const [roleIndex, setRoleIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRoleIndex((prevIndex) => (prevIndex + 1) % roles.length);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  });
+
   return (
     <section id="home" className="home">
       <div className="row">
         <div className="column col-6 text-container">
-          <h1 className="typewriter">Hi, I'm Kiran Dighe</h1>
-          <h4>Full-Stack Developer</h4>
+          <h1>Hi, I'm Kiran Dighe</h1>
+          <h4 style={{fontSize: '24px'}}>work as a <span className="changing-text">{roles[roleIndex]}</span></h4>
           <p>
             I am a passionate full-stack developer with 2 years of professional experience,
             currently working at BAAP Company. I am also in my third year, pursuing a BCA degree
--- src/pages/projects/Project.js
+++ src/pages/projects/Project.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { FaShoppingCart, FaChalkboardTeacher, FaProjectDiagram, FaUsers, FaBuilding, FaDatabase, FaTasks } from "react-icons/fa";
+import {
+  FaShoppingCart,
+  FaChalkboardTeacher,
+  FaProjectDiagram,
+  FaBuilding,
+  FaDatabase,
+  FaTasks,
+  FaTicketAlt,
+  FaLongArrowAltRight,
+} from "react-icons/fa";
 import "../../styles/Project.scss";
 
 function Projects() {
@@ -8,6 +17,7 @@ function Projects() {
       title: "Baap E-commerce Platform",
       description: "A scalable and user-friendly e-commerce platform designed for both small businesses and enterprises, offering advanced features like secure payments, inventory management, and analytics.",
       icon: <FaShoppingCart className="project-icon" />,
+      link: "https://commerce-dev.baap.market/"
     },
     {
       title: "Learning Management System",
@@ -20,24 +30,28 @@ function Projects() {
       icon: <FaProjectDiagram className="project-icon" />,
     },
     {
-      title: "Customer Relationship Management",
-      description: "A CRM system to streamline customer interactions, manage sales pipelines, and enhance team collaboration for better customer relationships.",
-      icon: <FaUsers className="project-icon" />,
-    },
-    {
       title: "Simplify VMS",
       description: "An efficient Visitor Management System (VMS) for tracking, registering, and managing visitor records seamlessly.",
       icon: <FaBuilding className="project-icon" />,
+      link: "https://dev-account.simplifysandbox.net"
     },
     {
       title: "Simplify DSaaS Web",
       description: "A cloud-based Data-as-a-Service (DSaaS) platform enabling efficient data management, storage, and analytics for modern businesses.",
       icon: <FaDatabase className="project-icon" />,
+      link: "https://keycloak.ingress-qa.simplifyvmsapp.com/auth/realms/dsaas-qa/protocol/openid-connect/auth?client_id=dsaas-enterprise-web&redirect_uri=https%3A%2F%2Fqa-enterprise-dsaas.simplifysandbox.net%2Fenterprise%2Fmy-candidate&state=54a3340c-dae1-4242-ad81-fa30bc6fd982&response_mode=fragment&response_type=code&scope=openid&nonce=1bd90397-5b17-4590-8330-772a63a672d1"
     },
     {
       title: "To-Do List Application",
       description: "A simple and sleek application to manage daily tasks with ease, featuring task prioritization, reminders, and progress tracking.",
       icon: <FaTasks className="project-icon" />,
+      link: "https://daily-task-management-a90ad.firebaseapp.com/",
+    },
+    {
+      title: "Event Ticketing Application",
+      description: "Events ticketing tools are systems that manage customer service and events tracking.",
+      icon: <FaTicketAlt className="project-icon" />,
+      link: "https://website-d45a5.firebaseapp.com/",
     },
   ];
 
@@ -50,6 +64,16 @@ function Projects() {
             <div className="project-icon-wrapper">{project.icon}</div>
             <h3 className="project-title">{project.title}</h3>
             <p className="project-description">{project.description}</p>
+
+            {project.link && (
+              <a
+                href={project.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project-arrow">
+                <FaLongArrowAltRight className="arrow-icon" />
+              </a>
+            )}
           </div>
         ))}
       </div>
